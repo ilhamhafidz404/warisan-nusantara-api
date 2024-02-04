b620db413fc65acd7ca9eada90c5dbead43cbfa7
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2024-02-04 22:52:36 +0700

    WN-23 show news

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -18,4 +18,13 @@ class NewsController extends Controller
             "status_code" => "WN-01"
         ]);
     }
+
+    public function show($id) {
+        $news = News::find($id);
+        return response()->json([
+            "data" => $news,
+            "message" => "Get data News success",
+            "status_code" => "WN-02"
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/berita", [NewsController::class, "index"]);
+Route::apiResource("/berita", NewsController::class);
